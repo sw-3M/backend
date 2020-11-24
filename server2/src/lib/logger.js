de0d54f3e316f3b831b419ecbbe99cd8a8e1908c
이원제 <50200484+leewonje418@users.noreply.github.com>
2020-11-24 10:00:16 +0900

    [UPDATE] logger추가 및 수정

---
+++ server2/src/lib/logger.js
@@ -0,0 +1,27 @@
+
+const Winston = require('winston');
+const WinstonDaily = require('winston-daily-rotate-file');
+
+module.exports = Winston.createLogger({
+  level: 'info',
+  showLevel: true,
+  format: Winston.format.combine(
+    Winston.format.json(),
+    Winston.format.timestamp({
+      format: 'YYYY-MM-DD HH:mm:ss',
+    }),
+    Winston.format.simple(),
+  ),
+  transports: [
+    new Winston.transports.File({
+      name: 'info-file',
+      filename: './logs/ServerLog.log',
+    }),
+
+    new WinstonDaily({
+      name: 'info-file',
+      filename: './logs/Date/Log_%DATE%.log',
+      datePattern: 'YYYY-MM-DD',
+    }),
+  ],
+});
